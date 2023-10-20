ecb6591d684bb9e8020b9df478bf9ed5f5dee04a
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-10-20 17:31:31 +0100

    add scss to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.scss'
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components';
 
 function App() {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path='' element={<Layout />}/>
+    </Routes>
+    </>
   )
 }
 
